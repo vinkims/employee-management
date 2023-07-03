da285f10ad51a4577739323980e0b64493f73319
vinkims <vincekigen@gmail.com>
2023-07-03 14:12:16 +0300

    chore(Setup)- Initial commit

---
+++ src/test/java/ke/kigen/employeemanagement/EmployeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package ke.kigen.employeemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
